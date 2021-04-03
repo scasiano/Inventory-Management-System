40c577d344af8a0c5d27cfc26c93c637d84c88f6
Shane Casiano <shane.casiano@gmail.com>
2021-04-02 20:23:17 -0400

    SQLConnector + Controller code
    -added SQLConntector Class to lib
    - Added SqlController
    - Started Products

--- InventoryManagement/src/ims/Products.java
+++ InventoryManagement/src/ims/Products.java
@@ -1,8 +1,37 @@
 package ims;
 
+import java.sql.*;
+import java.util.*;
+
 public class Products {
+
+    public static ResultSet SelectAll() throws SQLException {
+        ArrayList<String[]> dbResultList = new ArrayList<>();
+        ResultSet dbResultSet = SqlController.dbStatement.executeQuery("select * from products");
+        return dbResultSet;
+    }
+    public static ResultSet SelectProductId() throws SQLException {
+        ArrayList<String[]> dbResultList = new ArrayList<>();
+        ResultSet dbResultSet = SqlController.dbStatement.executeQuery("select product_id from products");
+        return dbResultSet;
+    }
+    public static ResultSet SelectName() throws SQLException {
+        ArrayList<String[]> dbResultList = new ArrayList<>();
+        ResultSet dbResultSet = SqlController.dbStatement.executeQuery("select name from products");
+        return dbResultSet;
+    }
+    public static ResultSet SelectMsrp() throws SQLException {
+        ArrayList<String[]> dbResultList = new ArrayList<>();
+        ResultSet dbResultSet = SqlController.dbStatement.executeQuery("select msrp from products");
+        return dbResultSet;
+    }
+    public static ResultSet SelectPrice() throws SQLException {
+        ArrayList<String[]> dbResultList = new ArrayList<>();
+        ResultSet dbResultSet = SqlController.dbStatement.executeQuery("select price from products");
+        return dbResultSet;
+    }
+
     //TODO: Add
     //TODO: Update
     //TODO: Delete
-    //TODO Read
 }
