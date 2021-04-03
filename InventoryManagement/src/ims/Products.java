d75a1c6e6ffa612dbfd28a91529db7091d89994f
Shane Casiano <shane.casiano@gmail.com>
2021-04-03 16:42:22 -0400

    Better return method, No sql needed on GUI

--- InventoryManagement/src/ims/Products.java
+++ InventoryManagement/src/ims/Products.java
@@ -4,21 +4,46 @@ import java.sql.*;
 import java.util.*;
 
 public class Products {
+    int productID;
+    String name;
+    double msrp;
+    double price;
 
-    public static ResultSet SelectAll() throws SQLException {
-        return SqlController.dbStatement.executeQuery("select * from products");
+    Products(int productID, String name, double msrp, double price){
+        this.productID = productID;
+        this.name = name;
+        this.msrp = msrp;
+        this.price = price;
     }
-    public static ResultSet SelectProductId() throws SQLException {
-        return SqlController.dbStatement.executeQuery("select product_id from products");
+    public static ArrayList<Products> SelectAll() throws SQLException {
+        ResultSet dbResult = SqlController.dbStatement.executeQuery("select * from products");
+        ArrayList<Products> resultList = new ArrayList<>();
+        while (dbResult.next()) resultList.add(new Products(dbResult.getInt(1), dbResult.getString(2), dbResult.getDouble(3), dbResult.getDouble(4)));
+        return resultList;
     }
-    public static ResultSet SelectName() throws SQLException {
-        return SqlController.dbStatement.executeQuery("select name from products");
+    public static ArrayList<Integer> SelectProductId() throws SQLException {
+        ResultSet dbResult = SqlController.dbStatement.executeQuery("select product_id from products");
+        ArrayList<Integer> resultList = new ArrayList<>();
+        while (dbResult.next()) resultList.add(dbResult.getInt(1));
+        return resultList;
     }
-    public static ResultSet SelectMsrp() throws SQLException {
-        return SqlController.dbStatement.executeQuery("select msrp from products");
+    public static ArrayList<String> SelectName() throws SQLException {
+        ResultSet dbResult = SqlController.dbStatement.executeQuery("select name from products");
+        ArrayList<String> resultList = new ArrayList<>();
+        while (dbResult.next()) resultList.add(dbResult.getString(1));
+        return resultList;
     }
-    public static ResultSet SelectPrice() throws SQLException {
-        return SqlController.dbStatement.executeQuery("select price from products");
+    public static ArrayList<Double> SelectMsrp() throws SQLException {
+        ResultSet dbResult = SqlController.dbStatement.executeQuery("select msrp from products");
+        ArrayList<Double> resultList = new ArrayList<>();
+        while (dbResult.next()) resultList.add(dbResult.getDouble(1));
+        return resultList;
+    }
+    public static ArrayList<Double> SelectPrice() throws SQLException {
+        ResultSet dbResult = SqlController.dbStatement.executeQuery("select price from products");
+        ArrayList<Double> resultList = new ArrayList<>();
+        while (dbResult.next()) resultList.add(dbResult.getDouble(1));
+        return resultList;
     }
 
     //TODO: Add
