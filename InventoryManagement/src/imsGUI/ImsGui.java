6fb04aaeb88fb819e4d12c0bd9162966f8d1c747
Js21914 <54334252+Js21914@users.noreply.github.com>
2021-04-08 11:50:09 -0400

    Product status + Other

--- InventoryManagement/src/imsGUI/ImsGui.java
+++ InventoryManagement/src/imsGUI/ImsGui.java
@@ -11,7 +11,7 @@ public class ImsGui extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Test.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Inventory Management Database");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
