58100d0fa839b583426e597765c611f98d41df8d
Shane Casiano <shane.casiano@gmail.com>
2021-04-07 21:02:22 -0400

    I hate the letter s

--- InventoryManagement/src/imsGUI/TestController.java
+++ InventoryManagement/src/imsGUI/TestController.java
@@ -1,22 +1,15 @@
 package imsGUI;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.*;
-import javafx.scene.layout.GridPane.*;
 import javafx.stage.Stage;
-import java.io.IOException;
 
 public class TestController {
     @FXML
     private void openProducts() throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("ProductsList.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("ProductList.fxml"));
         Stage stage = new Stage();
         stage.setTitle("Hello World");
         stage.setScene(new Scene(root, 300, 275));
