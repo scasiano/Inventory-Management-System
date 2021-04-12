56e04e41d239cd9c2fadf7036d030ae628f37ee4
Shane Casiano <shane.casiano@gmail.com>
2021-04-12 19:59:32 -0400

    Update SqlController.java

--- InventoryManagement/src/ims/SqlController.java
+++ InventoryManagement/src/ims/SqlController.java
@@ -5,9 +5,9 @@ import java.util.ArrayList;
 public class SqlController {
 
     //Database connection information
-    private static String dbUserName = "jayspy"; //Username for MySQL with rights to database
+    private static String dbUserName = "casiano"; //Username for MySQL with rights to database
     private static String dbPassword = "password"; //Password for MySQL user with rights to database
-    private static String dbIpAddress = "192.168.40.129"; //IP address the database is hosted at (probably localhost)
+    private static String dbIpAddress = "10.10.100.4"; //IP address the database is hosted at (probably localhost)
     private static String dbPort = "3306"; //Port of the database (probably 3306)
     private static String dbName = "inventory_management"; //Name of the database to modify
 
