20fd377113e23ca6472cb43d4c3ca86901c248bd
DuncanH15 <dhedden@morristechnology.com>
2021-04-15 12:26:47 -0400

    FXML set-up.

--- InventoryManagement/src/imsGUI/BudgetController.java
+++ InventoryManagement/src/imsGUI/BudgetController.java
@@ -1,4 +1,43 @@
 package imsGUI;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+
 public class BudgetController {
+
+    @FXML
+    Label periodLabel;
+    @FXML
+    Label netLabel;
+    @FXML
+    Label outLabel;
+    @FXML
+    Label inLabel;
+    @FXML
+    Label empLabel;
+    @FXML
+    DatePicker dateRange;
+    @FXML
+    TextField netProfit;
+    @FXML
+    TextField outgoing;
+    @FXML
+    TextField incoming;
+    @FXML
+    TextField userInfo;
+    @FXML
+    Button addBtn;
+    @FXML
+    Button modifyBtn;
+    @FXML
+    Button deleteBtn;
+    @FXML
+    HBox periodList;
+
+
+
 }
