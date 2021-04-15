5e4c57875992dc049484af40d31c9544fc354fe8
Dakarai Holcomb <dh06770@georgiasouthern.edu>
2021-04-15 17:43:09 -0400

    login method created giving a mysql error however and admin boolean needs to be global

--- InventoryManagement/src/imsGUI/LoginController.java
+++ InventoryManagement/src/imsGUI/LoginController.java
@@ -6,14 +6,36 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
+import ims.Users;
+import static ims.Users.authenticateUser;
 
 public class LoginController {
-    //once login is set up this should lead to homepage
+
     @FXML
-    private void openHomePage(ActionEvent event) {
-        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        stage.setScene(ImsGui.global.getHomepageScene());
-        stage.show();
+    TextField userBox;
+    @FXML
+    TextField passBox;
+    @FXML
+    Button LogBut;
+
+    public void Login() {
+        try {
+            String User = userBox.getText();
+            String Pass = passBox.getText();
+            Boolean admin;
+            String role = authenticateUser(User, Pass);
+            admin = role.equals("Admin");
+
+            Stage stage = (Stage) LogBut.getScene().getWindow();
+            stage.setScene(ImsGui.global.getHomepageScene());
+            stage.show();
+
+        } catch (Exception e) {
+            Global.exceptionAlert(e, "login");
+        }
     }
 }
+
+    //once login is set up this should lead to homepage
