789aae91616fd5a406a7773ab3e23fc20d9ba97e
Dakarai Holcomb <dh06770@georgiasouthern.edu>
2021-04-16 19:43:34 -0400

    added global admin and username

--- InventoryManagement/src/imsGUI/Global.java
+++ InventoryManagement/src/imsGUI/Global.java
@@ -26,6 +26,8 @@ public class Global {
     private Scene productListScene;
     private Scene productStatusScene;
     private Scene profilesScene;
+    private Boolean privilege;
+    private String currentUser;
 
     public void loadFxmlFiles() throws Exception{
         adminUserList = FXMLLoader.load(getClass().getResource("AdminUserList.fxml"));
