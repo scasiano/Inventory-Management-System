8375ba4d6affca4139d232db31f5a69dafe48967
Dakarai Holcomb <dh06770@georgiasouthern.edu>
2021-04-17 22:17:52 -0400

    order page created and initialized

--- InventoryManagement/src/imsGUI/OrdersController.java
+++ InventoryManagement/src/imsGUI/OrdersController.java
@@ -6,9 +6,41 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class OrdersController {
+
+    @FXML
+    HBox orderList;
+    @FXML
+    TextField orderID;
+    @FXML
+    TextField customerName;
+    @FXML
+    TextField customerAddress;
+    @FXML
+    TextField shippingStatus;
+    @FXML
+    TextField trackingID;
+    @FXML
+    TextField carrier;
+    @FXML
+    TextField employeeID;
+    @FXML
+    Button addOrder;
+    @FXML
+    Label addOrderLabel;
+    @FXML
+    HBox addOrderBox;
+    @FXML
+    HBox modBox;
+    @FXML
+    Button startMod;
+
     @FXML
     private void openHomePage(ActionEvent event) {
         Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
