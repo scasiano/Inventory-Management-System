12b19a32795b2aed6c0137a32a05a46411049953
Shane Casiano <shane.casiano@gmail.com>
2021-04-19 17:21:22 -0400

    more clean

--- InventoryManagement/src/imsGUI/HomepageController.java
+++ InventoryManagement/src/imsGUI/HomepageController.java
@@ -2,10 +2,7 @@ package imsGUI;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
@@ -26,7 +23,6 @@ public class HomepageController {
     public void initialize(){
        allowAdmin();
     }
-  //  public void logout(){    }
     private void allowAdmin(){
         if(!(Global.privilege)){
             adminB.setVisible(false);
