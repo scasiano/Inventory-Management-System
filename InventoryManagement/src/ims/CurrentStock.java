c2d7eed1c1ef0eee1dccbc26a3320ab4e8a331ba
Shane Casiano <shane.casiano@gmail.com>
2021-04-22 01:30:45 -0400

    oops

--- InventoryManagement/src/ims/CurrentStock.java
+++ InventoryManagement/src/ims/CurrentStock.java
@@ -5,10 +5,17 @@ import java.util.ArrayList;
 public class CurrentStock { //This entire table is autogen, all products exist here and quantity is changed based on incoming v. outgoing
     private long productID;
     private int quantity;
+    private String productName; //Not in DB
 
     public CurrentStock(long productID, int quantity) {
         this.productID = productID;
         this.quantity = quantity;
+    }
+
+    public CurrentStock(long productID, int quantity, String productName) {
+        this.productID = productID;
+        this.quantity = quantity;
+        this.productName = productName;
     }
 
     //Get set
@@ -18,12 +25,15 @@ public class CurrentStock { //This entire table is autogen, all products exist h
     public int getQuantity() {
         return quantity;
     }
+    public String getProductName() {
+        return productName;
+    }
 
     //SQL Queries READ
     public static ArrayList<CurrentStock> selectAll() throws SQLException {
         ResultSet dbResult = SqlController.dbStatement.executeQuery("select * from current_stock");
         ArrayList<CurrentStock> resultList = new ArrayList<>();
-        while (dbResult.next()) resultList.add(new CurrentStock(dbResult.getLong(1), dbResult.getInt(2)));
+        while (dbResult.next()) resultList.add(new CurrentStock(dbResult.getLong(1), dbResult.getInt(2), Products.selectProductNameByProductID(dbResult.getLong(1))));
         return resultList;
     }
     public static ArrayList<Long> selectProductID() throws SQLException {
