713a5572fddc20ff1bba08e33d5cca9a9ae2b64a
Shane Casiano <shane.casiano@gmail.com>
2021-04-22 12:19:37 -0400

    Revert "Clearing all selected info/saves/modifies after reloading page or making changes to DB."
    This reverts commit 8ec3543d532399be59b89f7d7fbd39e55d9d882f.

--- InventoryManagement/src/ims/SqlController.java
+++ InventoryManagement/src/ims/SqlController.java
@@ -3,14 +3,6 @@ import java.sql.*;
 import java.util.ArrayList;
 
 public class SqlController {
-
-    //Database connection information
-    private static String dbUserName = "duncan"; //Username for MySQL with rights to database
-    private static String dbPassword = "password"; //Password for MySQL user with rights to database
-    private static String dbIpAddress = "192.168.229.128"; //IP address the database is hosted at (probably localhost)
-    private static String dbPort = "3306"; //Port of the database (probably 3306)
-    private static String dbName = "inventory_management"; //Name of the database to modify
-
     //Database connection objects
     protected static Connection dbConnection;
     protected static Statement dbStatement;
