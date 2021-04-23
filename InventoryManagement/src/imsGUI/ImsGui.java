fa1ae1c1271f5c467ab361b137fb7e354c32d389
Shane Casiano <shane.casiano@gmail.com>
2021-04-22 23:17:10 -0400

    Update ImsGui.java

--- InventoryManagement/src/imsGUI/ImsGui.java
+++ InventoryManagement/src/imsGUI/ImsGui.java
@@ -7,6 +7,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class ImsGui extends Application {
@@ -29,13 +30,24 @@ public class ImsGui extends Application {
             BufferedReader lineRead = new BufferedReader(new FileReader("loginInformation"));
             Scanner objectRead = new Scanner(lineRead.readLine());
             objectRead.useDelimiter(",");
-            dbUserName = objectRead.next();
-            dbPassword = objectRead.next();
-            dbIpAddress = objectRead.next();
-            dbPort = objectRead.next();
-            dbName = objectRead.next();
+            try{
+                dbUserName = objectRead.next();
+                dbPassword = objectRead.next();
+                dbIpAddress = objectRead.next();
+                dbPort = objectRead.next();
+                dbName = objectRead.next();
+                lineRead.close();
+                objectRead.close();
+            }
+            catch (NoSuchElementException e){
+                Global.warningAlert("Error Reading File", "An error reading from loginInformation has occurred, please restart the program and fill in login information again.");
+                lineRead.close();
+                objectRead.close();
+                loginInformation.delete();
+                System.exit(4122);
+            }
             if (dbUserName == null || dbUserName.equals("") || dbPassword == null || dbPassword.equals("") || dbIpAddress == null || dbIpAddress.equals("") || dbPort == null || dbPort.equals("") || dbName == null || dbName.equals("")){
-                Global.warningAlert("User information incorrect", "Some portions of the user information was read as blank, please restart the program");
+                Global.warningAlert("Login Information Blank", "Login information for the DB cannot be blank.\nPlease restart the program and ensure that all fields are filled\nNote: if your database user does not use a password, you must set up a user with a password for this program.");
                 loginInformation.delete();
                 System.exit(4122);
             }
